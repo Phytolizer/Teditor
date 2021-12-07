b54d7dbd5f1b41d2ca5097b02da6ad36cc9a2fdf
Kyle Coffey <mariorules24@gmail.com>
2021-12-07 09:07:58 -0600

    colors

--- src/Teditor/Main.cpp
+++ src/Teditor/Main.cpp
@@ -12,7 +12,7 @@ int main()
 	sdl::TtfContext ttf = context.initTtf();
 	sdl::Window window = context.createWindow("Teditor", 800, 600, SDL_WINDOW_RESIZABLE);
 	sdl::Renderer renderer = window.createRenderer(-1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	sdl::FcFont font = renderer.loadFont("res/fonts/Hack.ttf", 16, {0, 0, 0, 255}, TTF_STYLE_NORMAL);
+	sdl::FcFont font = renderer.loadFont("res/fonts/Hack.ttf", 16, {255, 255, 255, 255}, TTF_STYLE_NORMAL);
 
 	rope* myRope = rope_new();
 	rope_insert(myRope, 0, reinterpret_cast<const uint8_t*>("Hello, world!"));
@@ -34,7 +34,7 @@ int main()
 			}
 		}
 
-		renderer.setDrawColor(SDL_Color{100, 0, 0, 255});
+		renderer.setDrawColor(SDL_Color{25, 25, 25, 255});
 		renderer.clear();
 		uint8_t* myText = rope_create_cstr(myRope);
 		font.draw(0, 0, reinterpret_cast<const char*>(myText));
