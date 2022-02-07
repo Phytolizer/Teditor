c528bdf2aff2ce6db3a413562403c2ba11f1ee42
Kyle Coffey <me@phytolizer.dev>
2022-02-06 21:17:38 -0600

    update formatting rules

--- main.c
+++ main.c
@@ -3,44 +3,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void scc(int code)
-{
-    if (code < 0)
-    {
+void scc(int code) {
+    if (code < 0) {
         fprintf(stderr, "SDL error: %s\n", SDL_GetError());
         exit(1);
     }
 }
 
-void* scp(void* ptr)
-{
-    if (ptr == NULL)
-    {
+void* scp(void* ptr) {
+    if (ptr == NULL) {
         fprintf(stderr, "SDL error: %s\n", SDL_GetError());
         exit(1);
     }
     return ptr;
 }
 
-int main(void)
-{
+int main(void) {
     scc(SDL_Init(SDL_INIT_VIDEO));
-    SDL_Window* win = scp(SDL_CreateWindow("Teditor",
-                                           SDL_WINDOWPOS_UNDEFINED,
-                                           SDL_WINDOWPOS_UNDEFINED,
-                                           800,
-                                           600,
-                                           SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE));
-    SDL_Renderer* ren = scp(SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC));
+    SDL_Window* win = scp(SDL_CreateWindow(
+        "Teditor", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600,
+        SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE));
+    SDL_Renderer* ren = scp(SDL_CreateRenderer(
+        win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC));
 
     bool quit = false;
-    while (!quit)
-    {
+    while (!quit) {
         SDL_Event event;
-        while (SDL_PollEvent(&event))
-        {
-            switch (event.type)
-            {
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
                 case SDL_QUIT:
                     quit = true;
                     break;
