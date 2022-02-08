3f961170ab4643a8b8bf5e546fed09c7ab54fd22
Kyle Coffey <me@phytolizer.dev>
2022-02-08 11:25:46 -0600

    AStyle

--- main.c
+++ main.c
@@ -15,20 +15,23 @@ void* scp(void* ptr) {
         fprintf(stderr, "SDL error: %s\n", SDL_GetError());
         exit(1);
     }
+
     return ptr;
 }
 
 int main(void) {
     scc(SDL_Init(SDL_INIT_VIDEO));
     SDL_Window* win = scp(SDL_CreateWindow(
-        "Teditor", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600,
-        SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE));
+                "Teditor", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600,
+                SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE));
     SDL_Renderer* ren = scp(SDL_CreateRenderer(
-        win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC));
+                win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC));
 
     bool quit = false;
+
     while (!quit) {
         SDL_Event event;
+
         while (SDL_PollEvent(&event)) {
             switch (event.type) {
                 case SDL_QUIT:
